b52cfb575e4aa3db4d15e973b9dd655ff52415b2
Aviraj Khare <avirajkhare00@gmail.com>
2017-09-17 09:16:27 +0530

    sending data to nodejs server

--- untitled/app1/views.py
+++ untitled/app1/views.py
@@ -3,6 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+import time
+import requests
+
+from app1.models import NewPersonObj
 
 # Create your views here.
 
@@ -10,6 +14,30 @@ from django.views.decorators.csrf import csrf_exempt
 def get_person_data(request):
 
     print request.POST
+
+    new_person = NewPersonObj()
+
+    timestamp = time.time()
+
+    new_person.timestamp = timestamp
+    new_person.save()
+
+    nodejs_url = "http://127.0.0.1:3004/pics_data"
+
+    data = {
+        "timestamp" : timestamp,
+        "image_data" : request.POST
+    }
+
+    r = requests.post(nodejs_url, data=data)
+
+    if r.status_code == 200:
+
+        print "successfully sent"
+
+    else:
+
+        print "debug step to send data to nodejs"
 
 
     return HttpResponse(200)
